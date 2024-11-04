4c575e827814af3de2642c371c9f27f66adb1f1b
bananafiesta <zhang.vctr@gmail.com>
2024-11-04 15:06:59 -0800

    adds link to avarian

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -55,15 +55,21 @@ export function MainPage() {
 
           <div id="projects" className="mb-5">
             <div className="rounded-md p-2 flex flex-col hover:bg-white/10 mb-5 hover:text-violet-400">
-              <p className="mb-2 font-bold">Avarian</p>
-              <p className="mb-2">Frontend webapp built to manage access to Dockerized backend services for friends, like game server maps, leaderboards, and upkeep information.</p>
-              <div className="flex flex-row gap-2 text-violet-200 font-semibold">
-                <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">React</p>
-                <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">Typescript</p>
-                <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">Javascript</p>
-                <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">Tailwind</p>
-              </div>
-
+              <a href="https://github.com/bananafiesta/avarian-client" target="_blank" rel="noreferrer">
+                <div className="flex flex-row items-center gap-2">
+                  <p className="mb-2 font-bold">Avarian</p>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 mb-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                  </svg>
+                </div>
+                <p className="mb-2">Frontend webapp built to manage access to Dockerized backend services for friends, like game server maps, leaderboards, and upkeep information.</p>
+                <div className="flex flex-row gap-2 text-violet-200 font-semibold">
+                  <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">React</p>
+                  <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">Typescript</p>
+                  <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">Javascript</p>
+                  <p className="bg-violet-500/70 rounded-full px-3 py-0.5 text-sm">Tailwind</p>
+                </div>
+              </a>
             </div>
 
             <div className="rounded-md p-2 flex flex-col hover:bg-white/10 mb-5 hover:text-violet-400">
